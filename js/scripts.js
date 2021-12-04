c2bf888cff9fa28121813345fc390d1e229328c9
Mariana <mariana.amfr@gmail.com>
2021-12-04 18:58:36 +0000

    Added information for four Pokémon to app

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('Hello, world!');
-const favoriteFood = "fries";
-document.write(favoriteFood);
+let pokemonList = [
+{ name: 'Marill', HP: 70, height: 0.4, types: ['water', 'fairy']},
+{ name: 'Beedrill', HP: 65, height: 1, types: ['bug', 'poison']},
+{ name: 'Squirtle', HP: 44, height: 0.5, types: ['water', 'poison']},
+{ name: 'Psyduck', HP: 50, height: 0.8, types: ['water', 'fairy']},
+];
