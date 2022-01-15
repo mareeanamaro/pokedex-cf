fb371409a742644fdb7bb4d1b8045ca20a46c018
Mariana <mariana.amfr@gmail.com>
2022-01-15 12:46:19 +0000

    Add bootstrap navbar and fix modals

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ let pokemonRepository = (function () {
   let apiUrl='https://pokeapi.co/api/v2/pokemon/?limit=150';
   let modal = document.querySelector("#detailsModal");
 
-  // create a function to add pokemon to the array
+  // create a function that will add pokemons to an array
   function add(pokemon){
     // the add function should only work if we are trying to add an object
     if (pokemon && typeof pokemon === 'object')  {
@@ -14,44 +14,6 @@ let pokemonRepository = (function () {
     else {
       console.log('This pokémon is not an object');
     }
-  }
-
-  //create a function to show the pokemons in a list of buttons
-  function addListItem(pokemon) {
-
-    // create a new variable that selects the pokemon list node
-    let pokemonList = document.querySelector('.pokemon-list');
-
-    // create list items and corresponding buttons
-    let listItem = document.createElement('li');
-
-    
-    // create the buttons and add the attributes to connect them to the modal
-    let pokemonButton = document.createElement('button');
-        pokemonButton.setAttribute('data-target', '#detailsModal');
-        pokemonButton.setAttribute('data-toggle', 'modal');
-
-        //add bootstrap utility class to buttons
-        pokemonButton.classList.add('btn');
-        pokemonButton.classList.add('btn-outline-secondary');
-
-        // add css to the buttons
-        pokemonButton.classList.add('list-button');
-
-
-    // make the button display the name of the pokemon
-    pokemonButton.innerText = pokemon.name;
-
-
-
-    // append the buttons to the list items and the list items to the list itself
-    listItem.appendChild(pokemonButton);
-    pokemonList.appendChild(listItem);
-
-    //add an event listener to the addListItem function
-    pokemonButton.addEventListener('click', function() {
-      showDetails(pokemon, modal);
-    });
   }
 
   // create a function that returns the array
@@ -70,7 +32,7 @@ let pokemonRepository = (function () {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
-          detailsUrl: item.url
+          detailsUrl: item.url,
         };
         add(pokemon);
       });
@@ -78,7 +40,52 @@ let pokemonRepository = (function () {
       console.error(e);
     }
   )
-}
+  }
+
+  //create a function to show the pokemons in a list of buttons
+  function addListItem(pokemon) {
+
+    // create a new variable that selects the pokemon list node
+    let pokemonList = document.querySelector('.pokemon-list');
+
+    // create list items and corresponding buttons
+    let listItem = document.createElement('li');
+
+
+    // create the buttons and add the attributes to connect them to the modal
+    let pokemonButton = document.createElement('button');
+        pokemonButton.setAttribute('data-target', '#detailsModal');
+        pokemonButton.setAttribute('data-toggle', 'modal');
+
+        //add bootstrap utility class to buttons and list items
+
+        listItem.classList.add('group-list-item');
+        listItem.classList.add('flex-fill');
+        pokemonButton.classList.add('btn');
+        pokemonButton.classList.add('btn-outline-secondary');
+
+        // add css to the buttons
+        pokemonButton.classList.add('list-button');
+
+    // make the button display the name of the pokemon
+    let imageElement = document.createElement('img');
+        imageElement.setAttribute('src', 'pokemon.sprites.front_default')
+        imageElement.classList.add('btn-img');
+
+        // ivan, podes ajudar com esta parte? não consigo que a imagem apareça corretamente
+        // pokemonButton.innerHTML = imageElement + pokemon.name;
+        pokemonButton.innerText = pokemon.name;
+
+    // append the buttons to the list items and the list items to the list itself
+    listItem.appendChild(pokemonButton);
+    pokemonList.appendChild(listItem);
+
+    //add an event listener to the addListItem function
+    pokemonButton.addEventListener('click', function() {
+      showDetails(pokemon, modal);
+    });
+  }
+
 
 // create a function that loads the detailed data for a given pokémon
 function loadDetails(pokemon) {
@@ -89,11 +96,10 @@ function loadDetails(pokemon) {
     pokemon.imageUrl = details.sprites.front_default;
     pokemon.height = details.height;
     pokemon.weight = details.weight;
-    pokemon.abilities = details.abilities;
-    pokemon.types = details.types.type;
-
+    pokemon.abilities = details.abilities.flatMap((element) => element.ability.name);
+    pokemon.types = details.types.flatMap((element) => element.type.name)
   }).catch(function (e) {
-    console.error(e);
+    console.error (e);
   });
 }
 
